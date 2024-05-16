e5caaf34e18299fb071c63332eefc72054bae00f
Jianing Xu <58917306+jianingxu1@users.noreply.github.com>
2024-05-16 14:53:04 +0200

    Add plot of metrics and saving

--- utils.py
+++ utils.py
@@ -65,3 +65,41 @@ def print_policy_for_taxi_env(policy):
     visual_help = {0: "v", 1: "^", 2: ">", 3: "<", 4: "P", 5: "D"}
     policy_arrows = [visual_help[x] for x in policy]
     print(np.array(policy_arrows).reshape([-1, 25]))
+
+
+def save_metric_plot(program_name: str, parameter_name: str, data: pd.DataFrame):
+    metrics = [
+        "average_time_per_episode",
+        "total_training_time",
+        "average_reward_obtained_test",
+    ]
+
+    for metric in metrics:
+        # Plot
+        plt.figure(figsize=(8, 6))
+        sns.barplot(
+            x=parameter_name,
+            y=metric,
+            hue=parameter_name,
+            palette="muted",
+            data=data,
+        )
+
+        # Add labels and title
+        plt.xlabel(f"Values for {parameter_name}")
+        plt.ylabel(f"{metric}")
+        plt.title(f"Comparison of {metric}\n for {parameter_name} parameter")
+        plt.legend(title=parameter_name)
+
+        if not os.path.exists(f"data"):
+            os.makedirs(f"data")
+
+        if not os.path.exists(f"data/{program_name}"):
+            os.makedirs(f"data/{program_name}")
+
+        if not os.path.exists(f"data/{program_name}/{parameter_name}"):
+            os.makedirs(f"data/{program_name}/{parameter_name}")
+
+        plt.savefig(
+            f"data/{program_name}/{parameter_name}/metric_{metric}_comparison_for_{parameter_name}.png"
+        )
