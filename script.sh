432c0c5b109f84b99051e95c445bedb587e3048e
Jianing Xu <58917306+jianingxu1@users.noreply.github.com>
2024-05-16 15:24:06 +0200

    Add experimentation script

---
+++ script.sh
@@ -0,0 +1,6 @@
+# To run QLearning experiments
+python3 qlearning.py gamma 0.5 0.75 0.95 0.99
+python3 qlearning.py epsilon 0.25 0.5 0.8 1.0
+python3 qlearning.py learning_rate 0.01 0.1 0.25 0.5
+python3 qlearning.py epsilon_decay 0.8 0.95 0.99 1.0
+python3 qlearning.py learning_rate_decay 0.8 0.95 0.99 1.0
